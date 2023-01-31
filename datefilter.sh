6f1c7096daeda9acb3efcd3540b66c8b3298024d
Moutsss <57500834+Tomalgmt@users.noreply.github.com>
2023-01-31 13:20:22 +0100

    Update datefilter.sh

--- datefilter.sh
+++ datefilter.sh
@@ -1,12 +1,15 @@
 awk -F';' -v start_date="$start_date" -v end_date="$end_date" '
 BEGIN {
   split(start_date, start, "-");
-  end_timestamp = mktime(end_date " 23 59 59");
-  start_timestamp = mktime(start_date " 00 00 00");
+  end_date = end_date " 23:59:59+0000";
+  split(end_date, end, "-");
+  start_timestamp = mktime(start[1] " " start[2] " " start[3] " 00 00 00");
+  end_timestamp = mktime(end[1] " " end[2] " " end[3] " 23 59 59");
 }
 {
   split($2, date, "T");
-  curr_timestamp = mktime(date[1] " " substr(date[2],1,8));
+  split(date[1], curr, "-");
+  curr_timestamp = mktime(curr[1] " " curr[2] " " curr[3] " " date[2]);
   if (curr_timestamp >= start_timestamp && curr_timestamp <= end_timestamp) {
     print;
   }
