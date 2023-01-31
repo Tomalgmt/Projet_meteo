0eb01ea62432c1d32e9110985fdfe5bd84b8c4eb
Moutsss <57500834+Tomalgmt@users.noreply.github.com>
2023-01-31 22:32:33 +0100

    Update datefilter.sh

--- datefilter.sh
+++ datefilter.sh
@@ -62,3 +62,29 @@ awk -F';' -v currdate_min1="$currdatemin_1" -v currdate_min2="$currdatemin_2" -v
 	    	}
 	    }
 	  }' meteo_filtered_data_v1.csv > date_filtered.csv
+	  
+V4
+
+# Extract the csv file
+csv_file="meteo_filtered_data_v1.csv"
+
+# Create the output file
+output_file="filtered_date.csv"
+
+# Create the lower bound of the date comparison
+curr_date_min=$curr_date_min_1"-"$curr_date_min_2"-"$curr_date_min_3"T"
+
+# Create the upper bound of the date comparison
+curr_date_max=$curr_date_max_1"-"$curr_date_max_2"-"$curr_date_max_3"T"
+
+# Read the file line by line
+while IFS= read -r line
+do
+    # Extract the date from the line
+    date=$(echo $line | cut -d, -f2)
+    # Check if the date is between the min and max
+    if [[ $date > $curr_date_min && $date < $curr_date_max ]]; then
+        # If it is, write the line to the output file
+        echo "$line" >> $output_file
+    fi
+done < $csv_file
