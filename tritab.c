def0d41d0d47b42b55a276b8db32a95049ea592f
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-02-02 21:35:36 +0100

    Update tritab.c

--- tritab.c
+++ tritab.c
@@ -8,85 +8,83 @@ typedef struct Chain{
 
 Chain* createChain(char e[255]){
 	Chain* a;
-	a =(Chain*)malloc(sizeof(Chain));
-	if (a==NULL){
+	a = (Chain*)malloc(sizeof(Chain));
+	if (a == NULL){
 		exit(1);
 	}
 	strcpy(a->val, e);
-	a->count=1;
-	a->next=NULL;
+	a->count = 1;
+	a->next = NULL;
 	return a;
 }
 
 Chain* insertStart(Chain* a, char e[255]){
-	Chain* new=createChain(e);
-	new->next=a;
+	Chain* new = createChain(e);
+	new->next = a;
 	return new;
 }
 
 Chain* insertChain (Chain* a, char e[255]){
-	Chain* b=createChain(e);
-	Chain* p1=a;
-	if (a==NULL){
-		a=b;
+	Chain* b = createChain(e);
+	Chain* p1 = a;
+	if (a == NULL){
+		a = b;
 	}
-	else if (strcmp(a->val,e)>0){
-		a=insertStart(a,e);
+	else if (strcmp(a->val ,e) > 0){
+		a = insertStart(a,e);
 		
 	}
-	else if (strcmp(a->val,e)<0){
-		while(p1->next!=NULL && strcmp(p1->next->val,e)<0){
-			//printf("a");
-			p1=p1->next;
+	else if (strcmp(a->val, e) < 0){
+		while (p1->next != NULL && strcmp(p1->next->val, e) < 0){
+			p1 = p1->next;
 		}
-		if (strcmp(a->val,e)==0){
-			a->count=a->count+1;
+		if (strcmp(a->val, e) == 0){
+			a->count = a->count + 1;
 			return a;
 		}
-		else if(p1->next==NULL){
-			p1->next=b;
+		else if(p1->next == NULL){
+			p1->next = b;
 		}
 		else {
-			b->next=p1->next;
-			p1->next=b;
+			b->next = p1->next;
+			p1->next = b;
 		}
 	}
 	else {
-		a->count=a->count+1;
+		a->count = a->count + 1;
 		return a;
 	}
 	return a;
 }
 
 Chain* insertChain_reverse (Chain* a, char e[255]){
-	Chain* b=createChain(e);
-	Chain* p1=a;
-	if (a==NULL){
-		a=b;
+	Chain* b = createChain(e);
+	Chain* p1 = a;
+	if (a == NULL){
+		a = b;
 	}
-	else if (strcmp(a->val,e)<0){
-		a=insertStart(a,e);
+	else if (strcmp(a->val, e) < 0){
+		a = insertStart(a,e);
 		
 	}
-	else if (strcmp(a->val,e)>0){
-		while(p1->next!=NULL && strcmp(p1->next->val,e)>0){
-			//printf("a");
-			p1=p1->next;
+	else if (strcmp(a->val, e) > 0){
+		while(p1->next != NULL && strcmp(p1->next->val, e) > 0){
+			p1 = p1->next;
 		}
-		if (strcmp(a->val,e)==0){
-			a->count=a->count+1;
+		if (strcmp(a->val, e) == 0){
+			a->count = a->count + 1;
 			return a;
 		}
-		else if(p1->next==NULL){
-			p1->next=b;
+		else if(p1->next == NULL){
+			p1->next = b;
 		}
 		else {
-			b->next=p1->next;
-			p1->next=b;
+			b->next = p1->next;
+			p1->next = b;
 		}
 	}
 	else {
-		a->count=a->count+1;
+		a->count = a->count + 1;
 		return a;
 	}
 	return a;
@@ -94,7 +92,7 @@ Chain* insertChain_reverse (Chain* a, char e[255]){
 
 void printChain(Chain* a, FILE *file){
 	if (a != NULL){
-		for (int i=0;i<a->count;i++){
+		for (int i=0; i < a->count; i++){
 			fprintf(file, "%s", a->val);
 		}
 		printChain(a->next, file);
@@ -105,7 +103,7 @@ void printChain(Chain* a, FILE *file){
 
 void printChain_reverse(Chain* a, FILE *file){
 	if (a != NULL){
-		for (int i=0;i<a->count;i++){
+		for (int i=0; i < a->count; i++){
 			fprintf(file, "%s", a->val);
 		}
 		printChain_reverse(a->next, file);
@@ -113,4 +111,3 @@ void printChain_reverse(Chain* a, FILE *file){
 		
 	}
 }
-
