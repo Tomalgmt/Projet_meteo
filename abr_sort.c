be90d83f9ccaa497b54105918bdbf1ad99010b05
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-02-03 05:10:55 +0100

    commentaires

--- abr_sort.c
+++ abr_sort.c
@@ -1,5 +1,7 @@
 #include"header.h"
 
+//------------------------[STRUCTURE]----------------------------
+
 typedef struct ABR{
 	char val[255];
 	int count;
@@ -7,9 +9,11 @@ typedef struct ABR{
 	struct ABR* right;
 }ABR;
 
+//-----------------------[FUNCTIONS]------------------------------
+
 ABR* createABR(char e[255]){
 	ABR* a;
-	a =(ABR*)malloc(sizeof(ABR));
+	a =(ABR*)malloc(sizeof(ABR));   //Creates the tree
 	strcpy(a->val, e);
 	a->count=1;
 	a->left = NULL;
@@ -25,7 +29,7 @@ ABR* insertABR (ABR* a, char e[255]){
 		a->left = insertABR (a->left, e);
 	}
 	else if (strcmp(e, a->val) > 0){
-		a->right = insertABR (a->right, e);
+		a->right = insertABR (a->right, e);  //Insert the new nodes into an existing tree
 	}
 	else {
 		a->count = a->count+1;
@@ -38,7 +42,7 @@ void printABR (ABR* a, FILE *file){
 	if (a != NULL){
 		printABR (a->left, file);
 		for (int i=0; i < a->count; i++){
-			fprintf(file, "%s", a->val);
+			fprintf(file, "%s", a->val);  //Print the sorted tree into the output file
 		}
 		printABR (a->right, file);
 		free(a);
@@ -50,7 +54,7 @@ void printABR_reverse(ABR* a, FILE *file){
 	if (a != NULL){
 		printABR_reverse (a->right, file);
 		for (int i=0; i<a->count; i++){
-			fprintf (file, "%s", a->val);
+			fprintf (file, "%s", a->val); //Print the sorted tree this time starting from the biggest value
 		}
 		printABR_reverse (a->left, file);
 		free(a);
