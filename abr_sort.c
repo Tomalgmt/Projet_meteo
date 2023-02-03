0682e9dae64abc32f377207a12ccb1a97bd02268
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-02-03 19:53:20 +0100

    Update abr_sort.c

--- abr_sort.c
+++ abr_sort.c
@@ -15,14 +15,14 @@ ABR* createABR(char e[255]){
 	ABR* a;
 	a =(ABR*)malloc(sizeof(ABR));   //Creates the tree
 	strcpy(a->val, e);
-	a->count=1;
+	a->count = 1;
 	a->left = NULL;
 	a->right = NULL;
 	return a;
 }
 
 ABR* insertABR (ABR* a, char e[255]){
-	if (a==NULL){
+	if (a == NULL){
 		return createABR(e);
 	}
 	if (strcmp(e, a->val) < 0){
@@ -32,7 +32,7 @@ ABR* insertABR (ABR* a, char e[255]){
 		a->right = insertABR (a->right, e);  //Insert the new nodes into an existing tree
 	}
 	else {
-		a->count = a->count+1;
+		a->count = a->count + 1;
 		return a;
 	}
 	return a;
@@ -50,7 +50,7 @@ void printABR (ABR* a, FILE *file){
 	}
 }
 
-void printABR_reverse(ABR* a, FILE *file){
+void printABR_reverse (ABR* a, FILE *file){
 	if (a != NULL){
 		printABR_reverse (a->right, file);
 		for (int i=0; i<a->count; i++){
