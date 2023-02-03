ae25b2ad286dd70cc3a0e9b3ed94e44a33e85b76
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-02-03 10:30:56 +0100

    Update t1.sh

--- t1.sh
+++ t1.sh
@@ -7,5 +7,5 @@
 		set ytics norangelimit logscale autofreq
 		set xlabel "Station"
 		set ylabel "Temperature"
-		plot "temperature.csv" u 1:3:4 w filledcurve fc rgb #80E0A080 title "Temperature", "temperature.csv" u 1:2 w smooth mcspline lw 2
+		plot "temperature.csv" u 1:3:4 w filledcurve fc rgb #80E0A080 title "Temperature min and max", "temperature.csv" u 1:2 w smooth mcspline lw 2 title "Average temperature"
 EOF
