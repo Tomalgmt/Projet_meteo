3dd88d572d47dc0ad2f7882361c359383bc402f6
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-02-03 04:33:12 +0100

    Update t1_p1.sh

--- t1_p1.sh
+++ t1_p1.sh
@@ -7,5 +7,5 @@
 		set ytics norangelimit logscale autofreq
 		set xlabel "Station"
 		set ylabel "Temperature"
-		plot "temperature.csv" u 1:3:4 w filledcurve title "Temperature", plot "temperature.csv" u 1:2 w smooth mcspline lw 2
+		plot "temperature.csv" u 1:3:4 w filledcurve title "Temperature", "temperature.csv" u 1:2 w smooth mcspline lw 2
 EOF
