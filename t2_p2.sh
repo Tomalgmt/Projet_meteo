f2539c9ae352a8d8d8452d71627f510713b35540
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-02-03 04:18:44 +0100

    Update t2_p2.sh

--- t2_p2.sh
+++ t2_p2.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+	gnuplot -p <<- EOF
+		set grid nopolar
+		set style data lines
+		set datafile separator ";"
+		set ytics norangelimit logscale autofreq
+		set xlabel "Station"
+		set ylabel "Temperature"
+		plot "temperature.csv" u 1:2 w smooth mcspline lw 2
+EOF
