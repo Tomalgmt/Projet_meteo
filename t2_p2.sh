a7235f6c035c26dac1f840d456dfb2a102550d35
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-02-03 18:25:59 +0100

    Update t2_p2.sh

--- t2_p2.sh
+++ t2_p2.sh
@@ -5,7 +5,7 @@
 		set style data lines
 		set datafile separator ";"
 		set ytics norangelimit logscale autofreq
-		set xlabel "Station"
+		set xlabel "Date"
 		set ylabel "Temperature"
-		plot "temperature.csv" u 1:2 w smooth mcspline lw 2
+		plot "temperature.csv" u 1:2 w line lw 3 lc rgb "navy" title "Average temperature"
 EOF
